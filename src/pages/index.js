ca702c1c8dc031acbc695a8ca8294feaec651141
sanliverpool13 <kairosh.sanjar@gmail.com>
2020-10-13 21:31:32 -0400

    Added Text in index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      Hello world!
+      <p>This Site is automatically built with github netlify actions</p>
+    </div>
+  )
 }
